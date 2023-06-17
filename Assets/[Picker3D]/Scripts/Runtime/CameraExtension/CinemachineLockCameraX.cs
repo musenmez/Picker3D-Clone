5878ceff5c40c971f38cb6e5048698e110cb65f7
Muhammed Üşenmez <59093336+musenmez@users.noreply.github.com>
2023-06-17 13:41:15 +0300

    Add Cinemachine extension

---
+++ Assets/[Picker3D]/Scripts/Runtime/CameraExtension/CinemachineLockCameraX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Picker3D.CameraExtension 
+{
+    using UnityEngine;
+    using Cinemachine;
+    
+    [SaveDuringPlay]
+    [AddComponentMenu("")] // Hide in menu
+    public class CinemachineLockCameraX : CinemachineExtension
+    {
+        [Tooltip("Lock the camera's X position to this value")]
+        [SerializeField] private float position;
+
+        protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+        {
+            if (stage == CinemachineCore.Stage.Finalize)
+            {
+                var pos = state.RawPosition;
+                pos.x = position;
+                state.RawPosition = pos;
+            }
+        }
+    }
+}
+
