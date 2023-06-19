65172e737893e147a54c7ed6f29a75ccaedb3fe8
Muhammed Üşenmez <59093336+musenmez@users.noreply.github.com>
2023-06-19 15:15:44 +0300

    Add Platform.cs

---
+++ Assets/[Picker3D]/Scripts/Runtime/Platforms/Platform.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Picker3D.Runtime 
+{
+    public class Platform : MonoBehaviour
+    {
+        [SerializeField] private MeshRenderer platformRenderer;
+        [SerializeField] private List<MeshRenderer> borderRenderes;
+
+        public Vector3 GetMaxPosition() 
+        {
+            Vector3 position = transform.position;
+            position.z = platformRenderer.bounds.max.z;
+            return position;
+        }
+
+        public Vector3 GetMinPosition()
+        {
+            Vector3 position = transform.position;
+            position.z = platformRenderer.bounds.min.z;
+            return position;
+        }
+
+        public void SetMaterials(Material groundMaterial, Material borderMaterial) 
+        {
+            platformRenderer.material = groundMaterial;
+            foreach (MeshRenderer borderRenderer in borderRenderes)
+            {
+                borderRenderer.material = borderMaterial;
+            }
+        }
+    }
+}
+
