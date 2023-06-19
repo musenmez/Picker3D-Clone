d4f16bae45125d76346dac7c797761505dd4fe0d
Muhammed Üşenmez <59093336+musenmez@users.noreply.github.com>
2023-06-19 15:18:40 +0300

    Add FinishLine.cs

---
+++ Assets/[Picker3D]/Scripts/Runtime/FinishLine/FinishLine.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Picker3D.Runtime 
+{
+    public class FinishLine : MonoBehaviour
+    {
+        
+    }
+}
+
