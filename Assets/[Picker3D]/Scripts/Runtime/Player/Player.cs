7c41ca5824fa8785f093a0158ea23f5867bacb4d
Muhammed Üşenmez <59093336+musenmez@users.noreply.github.com>
2023-06-19 15:19:58 +0300

    Update Player.cs

--- Assets/[Picker3D]/Scripts/Runtime/Player/Player.cs
+++ Assets/[Picker3D]/Scripts/Runtime/Player/Player.cs
@@ -8,41 +8,10 @@ namespace Picker3D.PlayerSystem
 {
     public class Player : MonoBehaviour
     {
-        public bool IsForwardMovementEnabled { get; private set; }
-        public bool IsSwerveEnabled { get; private set; }          
-
         private void Awake()
         {
-            PlayerManager.Instance.SetPlayer(this);
-            Initialize();
-        }
-
-        private void OnEnable()
-        {
-            PlayerManager.Instance.OnDepositStarted.AddListener(StartDeposit);
-            PlayerManager.Instance.OnDepositCompleted.AddListener(CompleteDeposit);
-        }
-
-        private void OnDisable()
-        {
-            PlayerManager.Instance.OnDepositStarted.RemoveListener(StartDeposit);
-            PlayerManager.Instance.OnDepositCompleted.RemoveListener(CompleteDeposit);
-        }        
-
-        private void Initialize() 
-        {
-            IsForwardMovementEnabled = false;
-        }
-
-        private void StartDeposit() 
-        {
-            IsForwardMovementEnabled = false;
-        }
-
-        private void CompleteDeposit() 
-        {
-            IsForwardMovementEnabled = true;
-        }
+            PlayerManager.Instance.SetPlayer(this);         
+        }       
     }
 }
 
