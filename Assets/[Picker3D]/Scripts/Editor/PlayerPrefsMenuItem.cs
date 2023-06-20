a3dec0b10a6cbace72f47aab6a9dfc4001a99e9e
Muhammed Üşenmez <59093336+musenmez@users.noreply.github.com>
2023-06-20 14:49:13 +0300

    Add PlayerPrefsMenuItem.cs

---
+++ Assets/[Picker3D]/Scripts/Editor/PlayerPrefsMenuItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Picker3D.EditorSystem 
+{
+    public class PlayerPrefsMenuItem
+    {
+        [MenuItem("Picker3D/Clear PlayerPrefs")]
+        private static void ClearPlayerPrefs()
+        {
+            PlayerPrefs.DeleteAll();
+        }
+    }
+}
+
