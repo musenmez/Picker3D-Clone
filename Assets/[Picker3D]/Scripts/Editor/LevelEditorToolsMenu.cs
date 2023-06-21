6a1e736c0f3da5922e0dda5ac3f5c09fccb466c3
Muhammed Üşenmez <59093336+musenmez@users.noreply.github.com>
2023-06-21 23:19:54 +0300

    Add LevelEditorToolsMenu.cs

---
+++ Assets/[Picker3D]/Scripts/Editor/LevelEditorToolsMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Picker3D.EditorSystem 
+{
+    [InitializeOnLoad]
+    public class LevelEditorToolsMenu : Editor
+    {
+        public static int SelectedTool { get; set; }
+        static string[] ButtonLabels { get; } = new string[] { "None", "Erase", "Paint" };
+
+        private const float GRID_WIDTH = 300f;
+
+        static LevelEditorToolsMenu() 
+        {
+            SceneView.duringSceneGui -= OnSceneGUI;
+            SceneView.duringSceneGui += OnSceneGUI;
+        }
+
+        static void OnSceneGUI(SceneView sceneView) 
+        {
+            DrawToolsMenu(sceneView);
+        }
+
+        static void DrawToolsMenu(SceneView sceneView) 
+        {
+            Handles.BeginGUI();
+           
+            GUILayout.BeginArea(new Rect(0, sceneView.position.height - 40, sceneView.position.width, 100), EditorStyles.toolbar);
+            {
+                SelectedTool = GUILayout.SelectionGrid(SelectedTool, ButtonLabels, 3, EditorStyles.toolbarButton, GUILayout.Width(GRID_WIDTH));
+            }
+            GUILayout.EndArea();
+
+            Handles.EndGUI();
+        }
+    }
+
+}
